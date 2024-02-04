adffd525ed2b258c52149881ec26c53db6b91984
Spritan <proyashpsb@gmail.com>
2024-02-04 19:25:01 +0530

    CLEAN: dir struct

--- client/UserClient/lib/features/core/controllers/tokens_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class TokensController extends GetxController {
-  static TokensController get instance => Get.find();
-
-  Rx<bool> isAchievementClicked = true.obs;
-}
